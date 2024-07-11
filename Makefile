1eadaa11699bfb1a0ff181e6630d9bc78a310e95
El-Prof-ai <142912104+El-Prof-ai@users.noreply.github.com>
2024-07-11 11:35:08 +0100

    initial commit

--- Makefile
+++ Makefile
@@ -1,16 +1,18 @@
 # Minimal makefile for Sphinx documentation
 
 # You can set these variables from the command line.
-SPHINXOPTS    =
-SPHINXBUILD   = sphinx-build
+SPHINXOPTS    ?=
+SPHINXBUILD   ?= sphinx-build
 SOURCEDIR     = .
-BUILDDIR      = $(shell if [ -n "$$READTHEDOCS" ]; then echo "$$READTHEDOCS_OUTPUT/html"; else echo "_build"; fi)
+BUILDDIR     = $(if $(READTHEDOCS_OUTPUT),$(READTHEDOCS_OUTPUT),_build)
 
 # Put it first so that "make" without argument is like "make help".
 help:
 	@$(SPHINXBUILD) -M help "$(SOURCEDIR)" "$(BUILDDIR)" $(SPHINXOPTS) $(O)
 
-html:
-	@$(SPHINXBUILD) -M html "$(SOURCEDIR)" "$(BUILDDIR)" $(SPHINXOPTS) $(O)
-	@echo
-	@echo "Build finished. The HTML pages are in $(BUILDDIR)/html."
+# Catch-all target: route all unknown targets to Sphinx using the new
+# "make mode" option.  $(O) is meant as a shortcut for $(SPHINXOPTS).
+%: Makefile
+	@$(SPHINXBUILD) -M $@ "$(SOURCEDIR)" "$(BUILDDIR)" $(SPHINXOPTS) $(O)
+
+.PHONY: help Makefile
--- conf.py
+++ conf.py
@@ -3,6 +3,10 @@ import sys
 
 sys.path.insert(0, os.path.abspath('.'))
 
+# Spécifiez le chemin vers votre répertoire source
+source_suffix = '.rst'
+master_doc = 'index'
+
 # Configuration file for the Sphinx documentation builder.
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
@@ -10,7 +14,7 @@ sys.path.insert(0, os.path.abspath('.'))
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'presentationConducteur'
+project = 'doc-conducteur '
 copyright = '2024, El_Prof'
 author = 'El_Prof'
 
@@ -22,11 +26,6 @@ extensions = [
     'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
 ]
-
-source_suffix = '.rst '
-
-# The master toctree document.
-master_doc = 'index'
 
 # The short X,Y version.
 version = ''
@@ -48,4 +47,13 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'  # Utilisez le thème ReadTheDocs
-html_static_path = ['_static']  # Ne pas inclure le répertoire de sortie ici
+
+# Configurez le chemin de sortie pour être compatible avec Read the Docs
+if os.environ.get('READTHEDOCS') == 'True':
+    output_path = os.environ.get('READTHEDOCS_OUTPUT', '_build')
+    html_output = os.path.join(output_path, 'html')
+else:
+    html_output = '_build/html'
+
+# Utilisez la variable html_output pour définir le chemin de sortie
+html_static_path = [os.path.join(html_output, '_static')]
