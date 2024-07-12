0c4d47ee500f344f0863a5e693d325bc98db527e
El-Prof-ai <142912104+El-Prof-ai@users.noreply.github.com>
2024-07-12 14:02:04 +0100

    pull_make_ok

--- pull_make.py
+++ pull_make.py
@@ -29,7 +29,10 @@ def make():
     
     try:
         print('make()')
-        time.sleep(40)
+        status_line = get_git_status()
+        while(status_line != 'Your branch is up to date with'):
+            time.sleep(3)
+            status_line = get_git_status()
         print('start make()........')
         # Obtenir le chemin du répertoire du script
         script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -51,8 +54,21 @@ def make():
     except Exception as e:
         logging.error('Une erreur est survenue: %s', str(e))
 
-#fdf
-
+def get_git_status():
+    # Exécute la commande `git status`
+    result = subprocess.run(['git', 'status'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    
+    # Vérifie si la commande s'est exécutée avec succès
+    if result.returncode != 0:
+        print(f"Erreur lors de l'exécution de git status: {result.stderr}")
+        return 'None'
+    
+    # Parcourt chaque ligne de la sortie pour trouver celle qui nous intéresse
+    for line in result.stdout.split('\n'):
+        if 'Your branch is up to date with' in line:
+            return 'Your branch is up to date with'
+    
+    return 'None'
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)  # Changer le port ici
